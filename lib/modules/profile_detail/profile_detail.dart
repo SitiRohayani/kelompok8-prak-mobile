11b3c4b9d64e21598fda884407587cd959314bf0
SitiRohayani <115017392+SitiRohayani@users.noreply.github.com>
2023-04-14 17:08:16 +0700

    tugas kel 8
    tugas kel 8

---
+++ lib/modules/profile_detail/profile_detail.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+
+class ProfilDetail extends StatefulWidget {
+  const ProfilDetail({super.key});
+
+
+  @override
+  State<ProfilDetail> createState() => _ProfilDetailState();
+}
+
+class _ProfilDetailState extends State<ProfilDetail> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
